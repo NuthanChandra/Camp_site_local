0480d88dd544bdd85651682bf4a5055aaa0f9c5e
NuthanChandra <nuthanchandra1997@gmail.com>
2018-12-01 18:05:23 +0530

    Add views directory with home.ejs and uninstall nodemon since it was not working

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-  res.send("Hello there boy");
+  res.render("home");
+});
+
+app.get("/campgrounds",function(req,res){
+  res.send("Nuthan Chandra");
 });
 app.listen(3000,function(){
   console.log("Server started");
