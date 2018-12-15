5ea2235eeb4b24f8a2023d0b3c0dd9b6f8c86b94
NuthanChandra <nuthanchandra1997@gmail.com>
2018-12-15 06:53:41 +0530

    Rename models/user to models/campground

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+var Campground = require("./models/campground.js");
 
 mongoose.connect("mongodb://127.0.0.1/camping_site");
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
-
-var User= require("./models/user.js");
-var campgrounds=[
-  {
-    name: "Nandi hills",
-    image: "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-exotica.jpg",
-    description: "Awesome place in karnataka"
-  },
-  {
-    name: "Rushikesh",
-    image: "https://www.holidify.com/blog/wp-content/uploads/2016/08/rishikesh-valley.jpg",
-    description: "very good place in up"
-  }
-];
-User.create(campgrounds,function(err,campground){
-  if(err){
-    console.log("Creating campgrounds failed");
-  }
-    else{
-      console.log("Saved campgrounds",campground);
-    }
-});
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
